f02e38c2a965eda70431298dc2989ea4c154cc86
Danielocooo <dsilbermann70@gmail.com>
2026-06-23 18:02:43 +0200

    Update field labels in forms.py to German

--- forms.py
+++ forms.py
@@ -1,14 +1,14 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Email, EqualTo
+from wtforms.validators import DataRequired, EqualTo
 
 class LoginForm (FlaskForm):
-    username = StringField ("E-Mail", validators=[DataRequired(), Email()])
+    username = StringField ("Benutzername", validators=[DataRequired()])
     password = PasswordField ("Passwort", validators =[DataRequired()])
     submit = SubmitField ("Anmelden")
 
 class RegistrationForm (FlaskForm):
-    email = StringField ("E-Mail", validators=[DataRequired(), Email()])
+    username = StringField ("Benutzername", validators=[DataRequired()])
     password = PasswordField ("Passwort", validators =[DataRequired()])
     password_confirm = PasswordField ("Passwort bestätigen", validators =[DataRequired(), EqualTo("password")])
     submit = SubmitField ("Konto erstellen")
